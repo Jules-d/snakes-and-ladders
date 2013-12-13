7800a767b0fa5df2f623f4c5810013eb551bb13a
Jules <julian.debhal@gmail.com>
2013-12-13 18:48:45 +1000

    Wip: single animated piece w dynamic code

--- src/snakes_and_ladders/core.clj
+++ src/snakes_and_ladders/core.clj
@@ -6,7 +6,6 @@
   (:gen-class)
   (:use clojure.test))
 
-
 ;; State
 
 ;; Snakes and Ladders does not need much state:
@@ -16,16 +15,21 @@
 (defn new-game
   ([] (new-game 2))
   ([number-of-players]
-     (let [state  [(vec (repeat number-of-players 1)) 0 0]]
+     (let [positions (vec (repeat number-of-players 1))
+           state  {:positions positions
+                   :previous-positions positions
+                   :move-times positions
+                   :animations (vec (repeat number-of-players (fn [args] args)))
+                   :turn-counter 0
+                   :player-turn 0}]
        state)))
 
 (def state-atom (atom (new-game 2)))
 
+(defn reset-game [players]
+  (swap! state-atom (fn [old-state] (new-game players))))
 
-(defn reset-game [players] (swap! state-atom (fn [old-state] (new-game players))))
-
-  ;; 3 players at positions 7 13 and 1, with player 3 about to move
-
+ ;; 3 players at positions 7 13 and 1, with player 3 about to move
 (def example-state [[7 13 4] 2 6])
 
 (def board boards/board)
@@ -42,33 +46,65 @@
        (println "Rolled a" current-roll)
        current-roll))
 
-(defn next-state [[positions current-player-id turn-counter]]
-  (let[current-player-position (positions current-player-id)
+(defn next-state [state]
+;;  (println "millis " (quil/millis))
+  (let[positions (state :positions)
+       current-player-id (state :player-turn)
+       turn-counter (state :turn-counter)
+       move-times (state :move-times)
+       animations (state :animations)
+       current-player-position (positions current-player-id)
        current-roll (roll-die)
        finish (board :finish)
+       time (quil/millis)
+       old-screen-position (drawing/position-to-screen-x-y current-player-position)
        rolled-position (+ current-player-position current-roll)
        new-position (min finish (shifted-position rolled-position))
-       new-positions (assoc positions current-player-id new-position)]
+       new-screen-x-y (drawing/position-to-screen-x-y new-position)
+       new-animation (drawing/generate-composable-dynamic-position
+                              {:start-time time
+                               :end-time (+ time 1250)
+                               :origin old-screen-position
+                               :destination new-screen-x-y})
+       new-positions (assoc positions current-player-id new-position)
+       new-move-times (assoc move-times current-player-id time)
+       new-animations (assoc animations current-player-id new-animation)
+       ]
     (if (= new-position finish)
-      (do ( println "Winner player" (inc current-player-id) "!!!")
+      (do (println "Winner player" (inc current-player-id) "!!!")
           [new-positions current-player-id turn-counter "Winner!"])
-      [new-positions (rem
-                      (inc current-player-id)
-                      (count positions))
-       (inc turn-counter)])))
+      (do ;;(println new-move-times)
+          {:positions new-positions
+           :previous-positions positions
+           :player-turn (rem (inc current-player-id) (count positions))
+           :turn-counter (inc turn-counter)
+           :move-times new-move-times
+           :animations new-animations
+           }))))
 
 (defn take-turn []
-  (swap! state-atom next-state))
+  (swap! state-atom next-state)
+  (drawing/do-animation))
 
 (defn draw []
-  ( drawing/draw @state-atom))
+  (drawing/draw @state-atom))
+
+;; Alternating control.  You can use these functions in defsketch.
+(defn key-turn []
+  (println (@state-atom 1) (= 0 (@state-atom 1)))
+  (when (= 0 (@state-atom 1)) (take-turn)))
+
+(defn mouse-turn []
+  (println (@state-atom 1) (= 0 (@state-atom 1)))
+  (when-not (= 0 (@state-atom 1)) (take-turn)))
 
 (quil/defsketch snakes-and-ladders-sketch
   :title "Snakes and Ladders"
   :setup drawing/setup
   :draw  draw
-  :size (:size boards/board) ;;TODO
-  :mouse-clicked take-turn)
+  :size (:size boards/board)
+  :mouse-pressed take-turn
+  :key-pressed take-turn)
 
 (defn -main [& args]
   (println "args - " args))
@@ -76,4 +112,4 @@
 ;; Control expressions
 (reset-game 2)
 
-(take-turn)
+;; (Take-turn)
--- src/snakes_and_ladders/quil.clj
+++ src/snakes_and_ladders/quil.clj
@@ -4,10 +4,11 @@
   (:require [snakes-and-ladders.boards :as boards]))
 
 (def img (ref nil))
+(def animation-duration 750)
 
 (defn setup []
   (smooth)                          ;;Turn on anti-aliasing
-  (frame-rate 30)                   ;;Set framerate to 1 FPS
+  (frame-rate 60)                   ;;Set framerate to 1 FPS
   (background 0)                    ;;Black background
   (dosync (ref-set img (load-image (:filename boards/board))))
   (set-state! :mouse-position (atom [0 0])))
@@ -55,25 +56,17 @@
 (defn wiggle
   [n] (+ n (* 50  (sin (* PI 2 (timer 2))))))
 
+(defn position-to-screen-x-y [position]
+  (column-row-to-screen-x-y (squarenum->column-row position)))
+
 (defn draw-player [player player-positions]
-  (let [[x y] ( column-row-to-screen-x-y (squarenum->column-row (player-positions player)))]
+  (let [[x y] ( position-to-screen-x-y (player-positions player))]
     (do
       (apply fill (player-token-color player))
       (apply ellipse (concat [x y] [token-size token-size])))))
 
 (def wip-origin [0 0])
 (def wip-destination [0 0])
-
-(defn draw [game-state]
-  (image @img 0 0)
-  (stroke 0 0 0)             ;;Set the stroke colour to a random grey
-  (stroke-weight 1)       ;;Set the stroke thickness randomly
-  (let [board boards/board
-        player-positions (game-state 0)]
-    (dorun (for [i (range (count player-positions))]
-             (do
-               (apply fill (player-token-color i))
-               (draw-player i player-positions))))))
 
 (defn mouse-moved [& args]
   (let [x (mouse-x)  y (mouse-y)]
@@ -83,11 +76,183 @@
 (defn progress [[start-time end-time] current-time]
   (let [duration (max 0 (- end-time start-time))
         progress (max 0 (- current-time start-time))]
-    (if (<= progress 0)
+    (if (or (= duration 0)
+            (<= progress 0))
       0
       (min  (/ progress duration) 1))))
 
-(is (= (progress [100 200] 100) 0 ))
-(is (= (progress [100 200] 150) 1/2 ))
-(is (= (progress [100 200] 200) 1 ))
-(is (= (progress [100 200] 300) 1 ))
+(def another-atom (atom 0))
+
+(def animation-atom (atom {:origin [0 0]
+                           :destination [150 150]
+                           :start 0
+                           :duration animation-duration}))
+
+(def animation-function-atom (atom (fn [] [100 100])))
+
+;; Usage:
+;;(update-animation @animation-atom :start 12 :origin [42 2] :destination [6 7])
+;;{:duration 0, :destination [6 7], :start 12, :origin [42 2]}
+(declare generate-dynamic-position)
+(declare generate-composable-dynamic-position)
+(defn update-animation []
+  (let [time (millis)
+        old-destination (@animation-atom :destination)
+        new-x (mouse-x)
+        new-y (mouse-y)
+        new-destination [new-x new-y]]
+    (do
+      (swap! animation-atom assoc
+             :origin old-destination
+             :destination new-destination
+             :start time)
+      (compare-and-set! animation-function-atom
+                        @animation-function-atom
+                        (generate-composable-dynamic-position {:start-time time
+                                                    :end-time (+ time 1250)
+                                                    :origin old-destination
+                                                    :destination new-destination}
+                                                   )))))
+
+(defn do-animation []
+;  (println "do-animationing at " (millis))
+  (compare-and-set! another-atom @another-atom (millis))
+  (update-animation))
+
+(defn pairwise [& args]
+  (apply map vector args))
+
+(defn interpolate [progress [a b]]
+  (let [ diff (- b a)]
+       (+ a (* progress diff))))
+
+(defn interpolate-vectors [progress origin destination]
+  ;; Convert [[x1 y1] [x2 y2]] to [[x1 x2] [y1 y2]]
+  (let [pairs (pairwise origin destination)]
+    (vec (map (partial interpolate progress) (pairwise origin destination)))))
+
+(is (= (interpolate-vectors 0.5 [0 0] [10 10]) [5.0 5.0]))
+(is (= (interpolate-vectors 1 [0 0] [10 10]) [10 10]))
+
+; An "animation" is just a value that varies by time: a function.
+(defn example-dynamic-position [time]
+  (let [start-time 0
+        minutes 1
+        end-time 3000
+        clamped-time (min end-time (max time start-time))
+        start-position 0
+        end-position 1000
+        current-progress (progress [start-time end-time] clamped-time)
+        result (interpolate current-progress [start-position end-position])]
+    result))
+
+(defn generate-dynamic-position [args]
+  (let [*start-time (args :start-time)
+        *end-time (args :end-time)
+        origin (args :origin)
+        destination (args :destination)]
+    (fn [time]
+      (let [start-time *start-time
+            end-time *end-time
+            clamped-time (min end-time (max time start-time))
+            start-position origin
+            end-position destination
+            current-progress (progress [start-time end-time] clamped-time)
+            result (interpolate current-progress
+                                [start-position end-position])]
+        result)
+      )))
+
+(def example-generated-dynamic-position
+  (generate-dynamic-position {:start-time 2000
+                              :end-time 7000
+                              :origin 100
+                              :destination 800}))
+
+(defn generate-composable-dynamic-position [args]
+  (let [*start-time (args :start-time)
+        *end-time (args :end-time)
+        origin (args :origin)
+        destination (args :destination)]
+    (fn [[time position]]
+      (let [start-time *start-time
+            end-time *end-time
+            clamped-time (min end-time (max time start-time))
+            start-position origin
+            end-position destination
+            current-progress (progress [start-time end-time] clamped-time)]
+        (if (or (< time start-time)
+                (> time end-time)) [time position]
+                [time (interpolate-vectors current-progress
+                                           start-position end-position)])))))
+
+
+(def example-composable-generated-dynamic-position
+  (generate-composable-dynamic-position {:start-time 1000
+                                         :end-time 11000
+                                         :origin [100 100]
+                                         :destination [1100 2100]}))
+
+
+(is (= (example-composable-generated-dynamic-position [0 [1 1]]) [0 [1 1]]))
+(is (= (example-composable-generated-dynamic-position [999 [2 2]]) [999 [2 2]]))
+(is (= (example-composable-generated-dynamic-position [1000 [2 2]]) [1000 [100 100]]))
+(is (= (example-composable-generated-dynamic-position [11000 [2 2]]) [11000 [1100 2100]]))
+(is (= (example-composable-generated-dynamic-position [11001 [2 2]]) [11001 [2 2]]))
+(is (= (example-composable-generated-dynamic-position [4000 [2 2]]) [4000 [400N 700N]]))
+(is (= (example-composable-generated-dynamic-position [7800 [2 2]]) [7800 [780N 1460N]]))
+
+
+
+(defn progress [[start end] value]
+  (let [span (- end start)
+        clamped-value (min (max value start) end)
+        elapsed (- clamped-value start)]
+    (if (= span 0)
+      0
+      (/ elapsed span))))
+
+(is (= (progress [0 100] 0) 0))
+(is (= (progress [0 100] 50) 1/2))
+(is (= (progress [0 100] 100) 1))
+
+(defn draw-animated-piece []
+  (fill 255 0 255)
+  (let [time (millis)
+        start (@animation-atom :start)
+        duration (max (@animation-atom :duration) 1)
+        origin (@animation-atom :origin)
+        destination (@animation-atom :destination)
+        animation-function @animation-function-atom
+        elapsed (max 0 (- time start))
+        progress (progress [start (+ start duration)] time)
+        [x y] (interpolate-vectors progress origin destination),
+        [returned-time dynamic-position]
+        (example-composable-generated-dynamic-position [time [destination]])]
+    (do
+      ;;(println progress " * " origin " -> " destination " : " position)
+      (ellipse
+       ;;(example-generated-dynamic-position time)
+       x
+        y token-size token-size)
+      ;(println (concat dynamic-position [60 60]))
+      ;;(apply ellipse (concat dynamic-position [60 60]))
+      )))
+
+(defn draw-players-dynamic [game-state]
+  nil)
+
+(defn draw [game-state]
+  (image @img 0 0)
+  (stroke 0 0 0)
+  (stroke-weight 2)
+  (let [board boards/board
+        player-positions (game-state :positions)
+        animations (:animations game-state)
+        time (quil.core/millis)]
+
+    (dorun (for [i (range (count player-positions))]
+             (do
+               (apply fill (player-token-color i))
+               (draw-player i player-positions)))))
+  (draw-animated-piece))
